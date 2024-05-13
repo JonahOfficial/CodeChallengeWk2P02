bd25dea7a3a0c7160f86bcf2ffff475333e85c54
Jonah Mwangala <jonahxnl@gmail.com>
2024-05-13 12:09:50 +0300

    Completed Lab

---
+++ bank-of-flatiron/components/Array.js
@@ -0,0 +1,46 @@
+const transactionsArray = [
+    {
+      id: 1,
+      date: "2024-04-01",
+      description: "Deposit: Salary",
+      category: "Income",
+      amount: 2500.00
+    },
+    {
+      id: 2,
+      date: "2024-04-05",
+      description: "Electric Bill Payment",
+      category: "Utilities",
+      amount: -80.00
+    },
+    {
+      id: 3,
+      date: "2024-04-10",
+      description: "Online Shopping: Clothing",
+      category: "Shopping",
+      amount: -120.50
+    },
+    {
+      id: 4,
+      date: "2024-04-15",
+      description: "Internet Bill Payment",
+      category: "Utilities",
+      amount: -60.00
+    },
+    {
+      id: 5,
+      date: "2024-04-20",
+      description: "Dinner at Restaurant",
+      category: "Food",
+      amount: -50.00
+    },
+    {
+      id: 6,
+      date: "2024-04-25",
+      description: "Gasoline",
+      category: "Transportation",
+      amount: -40.00
+    }
+  ];
+
+  export default transactionsArray;
